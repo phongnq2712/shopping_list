8e38e0d41f4ee1901f93042f13847c2b0bdbe433
phongnq <phongnq2712@gmail.com>
2021-10-29 15:21:50 -0400

    design layout

--- QuocPhongNgo_MAPD714_Midterm/QuocPhongNgo_MAPD714_Midterm/ViewController.swift
+++ QuocPhongNgo_MAPD714_Midterm/QuocPhongNgo_MAPD714_Midterm/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  QuocPhongNgo_MAPD714_Midterm
-//
-//  Created by Phong on 27/10/2021.
-//
+/**
+ * File Name: ViewController
+ * Author's Name: Quoc Phong Ngo
+ * Student ID: 301148406
+ * Date: October 27th, 2021
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var lblListName: UILabel!
+    @IBOutlet weak var textFieldListName: UITextField!
+    //    @IBOutlet weak var lblListName: UILabel!
+//    @IBOutlet weak var textFieldListName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        for textField in self.view.subviews where textField is UITextField {
+//            textField.resignFirstResponder()
+//        }
+        textFieldListName.text = nil
+        textFieldListName.placeholder = "Enter New List Name"
+        lblListName.text = "My Shopping List"
     }
-
-
+    
+    @IBAction func texFieldListNameChanged(_ sender: UITextField) {
+        lblListName.text = textFieldListName.text
+    }
 }
 
